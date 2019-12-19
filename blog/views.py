1381324a9138bac67721b82109fafb382f74311b
Hans Hendrickx <hhlhendrickx@gmail.com>
2019-12-19 16:06:05 +0100

    Blog with test
    All seems OK so far!

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView  # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
+from django.urls import reverse_lazy  # new
 
 from .models import Post
 
@@ -15,7 +16,19 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
-class BlogCreateView(CreateView):  # new
+class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):  # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
