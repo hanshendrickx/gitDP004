5aa333ec355107f6583fee150c6d29ca7943a501
Hans Hendrickx <hhlhendrickx@gmail.com>
2019-12-20 10:26:46 +0100

    Accounts working as well
    Sign Up, Log In, Log Out

--- mposp/urls.py
+++ mposp/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# mposp/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # new
+    path('accounts/', include('django.contrib.auth.urls')),  # new
+    path('accounts/', include('accounts.urls')),  # new
+    path('', include('blog.urls')),
 ]
